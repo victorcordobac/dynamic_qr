99aed8ce144e19b6f9d745b34f82f44a084a72af
victorcordoba <hola@victorcordoba.com>
2021-08-05 23:05:20 +0200

    prueba SWITCH

--- qrcode/forms/edit_dynamic_form.php
+++ qrcode/forms/edit_dynamic_form.php
@@ -1,4 +1,31 @@
 <fieldset>
+
+    <!--div class="col-sm-4">
+        <label for="state">Redirigir a URL</label>
+        
+        <div class="form-group">
+            <label class="radio-inline">
+            <input type="radio" name="state" value="enable" <?php echo ($edit &&$dynamic_qrcode['state'] =='enable') ? "checked": "" ; ?> required="required" id="enable"/> Activar</label>
+            
+            <label class="radio-inline">
+            <input type="radio" name="state" value="disable" <?php echo ($edit && $dynamic_qrcode['state'] =='disable')? "checked": "" ; ?> required="required" id="disable"/> Desactivar</label>
+        </div>
+    </div-->
+
+    <div class="col-sm-4">
+        <label for="state">Activado</label>
+        <div class="form-group bootstrap-switch bootstrap-switch-wrapper bootstrap-switch-focused bootstrap-switch-animate bootstrap-switch-off" style="width: 86px;">
+            <div class="bootstrap-switch-container" style="width: 126px; margin-left: -42px;">
+                <span class="bootstrap-switch-handle-on bootstrap-switch-success" style="width: 42px;" value="enable" id="enable">ON</span>
+                <span class="bootstrap-switch-label" style="width: 42px;">&nbsp;</span>
+                <span class="bootstrap-switch-handle-off bootstrap-switch-danger" style="width: 42px;" value="disable" id="disable">OFF</span>
+                <input type="checkbox" name="state
+                
+                " checked="<?php echo ($edit &&$dynamic_qrcode['state'] =='enable') ? "true": "false" ; ?>" data-bootstrap-switch="" data-off-color="danger" data-on-color="success">
+            </div>
+        </div>
+    </div>
+
     <div class="col-sm-4">
         <div class="form-group">
             <label for="identifier">Redirect identifier</label>
@@ -22,16 +49,4 @@
         </div>
     </div>
     
-
-    <div class="col-sm-4">
-        <label for="state">Redirigir a URL *</label>
-        
-        <div class="form-group">
-            <label class="radio-inline">
-            <input type="radio" name="state" value="enable" <?php echo ($edit &&$dynamic_qrcode['state'] =='enable') ? "checked": "" ; ?> required="required" id="enable"/> Activar</label>
-            
-            <label class="radio-inline">
-            <input type="radio" name="state" value="disable" <?php echo ($edit && $dynamic_qrcode['state'] =='disable')? "checked": "" ; ?> required="required" id="disable"/> Desactivar</label>
-        </div>
-    </div>
 </fieldset>
