17137e45f597291a1336f0f6b608a351ddf633dc
Umer Farooq <zerokool_007@yahoo.com>
2021-08-06 18:30:29 +0400

    Dynamic Default QR, History and Profile page with tabs created

--- qrcode/build/config/postcss.config.js
+++ qrcode/build/config/postcss.config.js
@@ -1,14 +1,14 @@
 'use strict'
 
-module.exports = {
-  map: {
+module.exports = (ctx) => ({
+  map: ctx.file.dirname.includes('examples') ? false : {
     inline: false,
     annotation: true,
     sourcesContent: true
   },
-  plugins: [
-    require('autoprefixer')({
+  plugins: {
+    autoprefixer: {
       cascade: false
-    })
-  ]
-}
+    }
+  }
+})
